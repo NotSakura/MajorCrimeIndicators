3e19eaf32cccd1edf6024f73590a9862846afffb
Sakura Noskor <sakura.noskor@mail.utoronto.ca>
2024-09-20 23:14:16 -0400

    initial commit plus set up such as writing scripts and cleaning data

---
+++ scripts/01-download_data.R
@@ -0,0 +1,38 @@
+#### Preamble ####
+# Purpose: Simulates Shelter occupancy data
+# Author: Shreya Sakura Noskor
+# Date: 09-18-2024
+# Contact: sakura.noskor@mail.utoronto.ca
+
+#### Workspace setup ####
+library(tidyverse)
+library(opendatatoronto)
+
+#### Download data ####
+# [...ADD CODE HERE TO DOWNLOAD...]
+
+
+
+#### Save data ####
+
+# get package
+package <- show_package("major-crime-indicators")
+package
+
+# get all resources for this package
+resources <- list_package_resources("major-crime-indicators")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
+
+## don't run as it already exists in repository
+# write_csv(
+#   x = data,
+#   file = "data/raw_data/MCI_rates.csv"
+# )
+
+         
---
+++ scripts/02-data_cleaning.R
@@ -0,0 +1,27 @@
+#### Preamble ####
+# Purpose: Simulates Shelter occupancy data
+# Author: Shreya Sakura Noskor
+# Date: 09-18-2024
+# Contact: sakura.noskor@mail.utoronto.ca
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Clean data ####
+raw_data <-
+  read_csv(
+    "../data/raw_data/MCI_rates.csv",
+    show_col_types = FALSE
+  )
+
+
+cleaned_data <- data %>%
+  select(REPORT_DATE, OCC_DATE, DIVISION, LOCATION_TYPE, MCI_CATEGORY, HOOD_158) |>
+  mutate(REPORT_DATE = as.Date(REPORT_DATE),
+         OCC_DATE = as.Date(OCC_DATE),
+         DATE_DIFF = as.integer(REPORT_DATE-OCC_DATE)) |>
+  drop_na()
+
+
+#### Save data ####
+write_csv(cleaned_data, "data/analysis_data/cleaned_data.csv")
